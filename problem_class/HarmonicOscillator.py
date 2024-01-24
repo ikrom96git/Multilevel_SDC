f87235d67299998ee582b51062ba6496c79fbc6b
Ikrom Akramov <ikrom.akramov@tuhh.de>
2024-01-24 16:40:11 +0100

    harmonic oscillator without force

--- problem_class/HarmonicOscillator.py
+++ problem_class/HarmonicOscillator.py
@@ -1,57 +1,89 @@
+
 import numpy as np
-import matplotlib.pyplot as plt
 from core.Pars import _Pars
+
+
 class HarmonicOscillator:
+
     def __init__(self, problem_params):
-        self.params=_Pars(problem_params)
-        self.determinant=self.params.mu**2*0.25-self.params.kappa
-    def get_righthandside(self):
+        """
 
-        pass
 
-    def get_exact_freeOscillation(self, time:float)->np.ndarray:
+        Parameters
+        ----------
+        problem_params : TYPE
+            DESCRIPTION.
 
-        if self.params.mu==0 and self.params.kappa>0.0:
-            omega=np.sqrt(self.params.kappa)
-            pos_withoutFriction=a0*np.cos(omega*time)+b0*np.sin(omega*time)
-            vel_withoutFriction=-omega*a0*np.sin(omega*time)+omega*b0*np.cos(omega*time)
-            return [pos_withoutFriction, vel_withoutFriction]
+        Returns
+        -------
+        None.
 
-        elif self.params.mu>0 and self.params.kappa>0:
-            Determinant=self.params.mu**2*0.25-self.params.kappa
-            exponent=np.exp(self.params.mu*0.5*time)
-            if Determinant<0:
-                omegaTilde=np.sqrt(-Determinant)
-                pos_withFriction=exponent*(a0*np.cos(omegaTilde*time)+b0*np.sin(omegaTilde*time))
-                vel_withFriction=exponent*(a0*np.cos(omegaTilde*time)+b0*np.sin(omegaTilde*time))
+        """
+        self.params = _Pars(problem_params)
+        self.determinant = self.params.mu**2*0.25-self.params.kappa
 
-            elif Determinant>0:
-                omegaTilde=np.sqrt(Determinant)
-                pos_withFriction=exponent*(a0*np.cos(omegaTilde*time)+b0*np.sin(omegaTilde*time))
-                vel_withFriction=exponent*(a0*np.cos(omegaTilde*time)+b0*np.sin(omegaTilde*time))
-
-            elif Determinant==0:
-                pos_withFriction=exponent*(a0+b0*time)
-                vel_withFriction=exponent*(a0+b0*time)
-            return [pos_withFriction, vel_withFriction]
-        else:
-            reise ValueError("The parameters are not valid for a harmonic oscillator.")
-    def get_exponent(self, t):
-        return np.exp(-0.5*self.mu*t)
     def get_cos(self, omega, t):
         return np.cos(omega*t)
+
     def get_sin(self, omega, t):
-        return np.sin(omega*t)
+        return np.sin(omega, t)
+
     def get_dcos(self, omega, t):
-        return -omega*np.sin(omega, t)
+        return -omega*np.sin(omega*t)
+
     def get_dsin(self, omega, t):
-        return omega * np.cos(omega*t)
-    def get_const(self, u0, omega, t):
-        pass
-    def get_determinantNegative(self, t):
-        omegaTilde=np.sqrt(-self.determinant)
-        pos_withFriction=self.get_exponent(t)*(a0*np.cos(omegaTilde*t)+b0*np.sin(omegaTilde*t))
-        vel_withFriction=
+        return omega*np.cos(omega*t)
 
+    def get_exp(self, omega,  t):
+        return np.exp(omega*t)
 
+    def get_dexp(self, omega, t):
+        return omega*self.get_exp(omega, t)
 
+    def const_withoutFriction(self, t):
+        omega = np.sqrt(self.params.kappa)
+        A = np.zeros((2, 2))
+        A[0, 0], A[0, 1] = self.get_cos(omega, t), self.get_sin(omega, t)
+        A[1, 0], A[1, 1] = self.get_dcos(omega, t), self.get_dsin(omega, t)
+        b = np.linalg.sove(A, self.params.u0)
+        return A, b
+
+    def const_withFriction(self, t):
+        A = np.zeros((2, 2))
+        omega_exp = -0.5*self.params.mu
+        exp = self.get_exp(omega_exp, t)
+        dexp = self.get_dexp(omega_exp, t)
+        if self.determinant < 0:
+            omega = np.sqrt(-self.determinant)
+            A[0, 0], A[0, 1] = exp * \
+                self.get_cos(omega, t), exp*self.get_sin(omega, t)
+            A[1, 0] = dexp*self.get_cos(omega, t)+exp*self.get_dcos(omega, t)
+            A[1, 1] = dexp*self.get_sin(omega, t)+exp*self.get_dsin(omega, t)
+
+        if self.determinant > 0:
+            omega = np.sqrt(self.determinant)
+            A[0, 0], A[0, 1] = exp * \
+                self.get_exp(omega, t), exp*self.get_exp(-omega, t)
+            A[1, 0] = dexp*self.get_exp(omega, t)+exp*self.get_dexp(omega, t)
+            A[1, 1] = dexp*self.get_exp(-omega, t)+exp*self.get_dexp(-omega, t)
+
+        if self.determinant == 0:
+            A[0, 0], A[0, 1] = exp, t*exp
+            A[1, 0], A[1, 1] = dexp, t*dexp+exp
+        b = np.linalg.solve(A, self.params.u0)
+        return A, b
+
+    def get_solution(self, t):
+        if self.mu == 0:
+            *_, b = self.const_withoutFriction(self.params.t0)
+            A, *_ = self.const_withoutFriction(t)
+        if self.mu > 0:
+            *_, b = self.const_withFriction(self.params.t0)
+            A, *_ = self.const_withFriction(t)
+        return A@b
+
+    def get_solution_interval(self, time: np.narray) -> np.narray:
+        solution_store = np.zeros([2, len(time)])
+        for tt in range(time):
+            solution_store[:, tt] = self.get_solution(tt)
+        return solution_store
